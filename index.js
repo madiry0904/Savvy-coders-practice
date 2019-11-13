4df6a102f3c1dad28e5bef78db8f9401b4d2a26e
Madi Ryan <madi.ryan92@gmail.com>
2019-11-12 21:06:05 -0600

    arguments v. parameters

--- index.js
+++ index.js
@@ -1,68 +1,8 @@
-//doMath is expecting to receive a function
-//operation can be any named parameter as long as it's referenced in return statement
-//functions can receive another function aka function doMath is receiving the operation funciton that is called in the return function
-/*function doMath(x, y, operation) {
-  console.trace();
-  return operation(x, y);
-}
-function subtract(x, y) {
-  console.trace();
-  return y - x;
+// a and b are NAMED PARAMETERS in this FUNCTION DECLARATION
+
+function myFunc(a, b) {
+  return a + b;
 }
 
-//function add(x, y) {
-//console.trace();
-//return y + x;
-// }
-
-console.log(doMath(10, 5, subtract));
-//console.log(doMath(10, 5, add));
-*/
-
-//This is a function factory that returns another function
-//This fucntion factory uses the concept of a CLOSURE
-/*this is the outer function*/ function initSportsScorer(pts) {
-  /*this is the inner function */ return function(score) {
-    return score + pts;
-  };
-}
-
-const threePointer = initSportsScorer(3); //the 3 is referencing "pts"
-console.log(threePointer(99)); //the 55 is referencing the score
-
-//answer is 102
-
-function rugbyScore(tri) {
-  return function(score) {
-    return score + tri;
-  };
-}
-
-const englandScore = rugbyScore(3);
-console.log(englandScore(14));
-
-function foo() {
-  console.trace();
-  bar();
-  console.trace();
-
-  return "foo";
-}
-
-function bar() {
-  console.trace();
-  baz();
-  console.trace();
-
-  return "bar";
-}
-
-function baz() {
-  console.trace();
-
-  return "baz";
-}
-
-console.log(foo());
-
-//"last in first out of the call stack" means were in the call stack with functions calling other functions and the last function called in the stack in the first one out of the stack
+//2 and 3 are ARGUMENTS that are passed upon FUNCTION INVOCATION
+console.log(myFunc(2, 3));
