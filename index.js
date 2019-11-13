9891d0161753351e369ecc8dd5d6ad5b63e2beb9
Madi Ryan <madi.ryan92@gmail.com>
2019-11-12 21:21:25 -0600

    Expressions v. statements & logical operators

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-// a and b are NAMED PARAMETERS in this FUNCTION DECLARATION
+/*EXPRESSION v. STATEMENTS
+expressions need to be part of a statement to be meaningful ex. (2+3 won't have meaning unless tied to a statement)
+const x = 2 + 3;
+const y = 2 === 3;
 
-function myFunc(a, b) {
-  return a + b;
-}
+Expression are used in conjunction with keywords and appropriate syntax to do useful things
+Expressions and operators-arithmetic left to right associativity */
 
-//2 and 3 are ARGUMENTS that are passed upon FUNCTION INVOCATION
-console.log(myFunc(2, 3));
+// LOGICAL OPERATORS involve and/ or operators
+// && (AND) both left and right operands must be true
+// ||(OR) checks if left operand is true, right is never seen-only checks if one is true
+//short circuiting is when an OR statement is evaluated and if the L is true then the R will never be evaluated
+const x = 3;
+const y = 5;
+const z = 10;
+
+console.log(x < y && x < z);
+console.log(x < y || x < z);
