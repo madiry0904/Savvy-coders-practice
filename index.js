15095d02d33424fc54e43b141cbee16e07f956ac
Madi Ryan <madi.ryan92@gmail.com>
2019-11-13 18:30:31 -0600

    HW 4 answers

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-function highOrLow(x, y) {
-  if (x > y) {
-    return "x is more";
-  }
-  return "y is more";
+/*We went over function factories in class today. Now, you can create one that will return a function that will include a 'greeting' and a 'name.'
+
+Here's how I want to be able to use it:
+
+const morningMartin = initGreeting('Good Morning', 'Martin');
+console.log(morningMartin()); // 'Good Morning, Martin!'
+
+So, I pass in 2 strings. First is a greeting next is a name. You have a function factory called initGreeting(greeting, name) . As you see 👆🏾, it return s a function that, when invoked, will return a string that basically just puts together the greeting and the `name`. */
+
+function initGreeting(greeting, name) {
+  return function(message) {
+    return greeting + name + message;
+  };
 }
-console.log(highOrLow(3, 5));
+
+const morningMartin = initGreeting("Good Morning ", "Martin!");
+console.log(morningMartin(` How are you today?`));
