76f88ba9c48141e99b2b8bea878fe069431dfd29
Madi Ryan <madi.ryan92@gmail.com>
2019-11-14 18:52:02 -0600

    homework 4 solutions

--- index.js
+++ index.js
@@ -7,11 +7,23 @@ console.log(morningMartin()); // 'Good Morning, Martin!'
 
 So, I pass in 2 strings. First is a greeting next is a name. You have a function factory called initGreeting(greeting, name) . As you see 👆🏾, it return s a function that, when invoked, will return a string that basically just puts together the greeting and the `name`. */
 
-function initGreeting(greeting, name) {
-  return function(message) {
-    return greeting + name + message;
+/*function initGreeting(greeting, name) {
+  return function() {
+    return `${greeting} ${name}`;
   };
 }
 
 const morningMartin = initGreeting("Good Morning ", "Martin!");
 console.log(morningMartin(` How are you today?`));
+*/
+
+//passing a parameter to print the time
+function initGreeting(greeting, name) {
+  return function(time) {
+    return `${greeting} ${name}. The time is ${time}`;
+  };
+}
+
+const morningMartin = initGreeting("Good Morning ", "Martin!");
+const time = new Date(Date.now());
+console.log(morningMartin(time.getHours()));
