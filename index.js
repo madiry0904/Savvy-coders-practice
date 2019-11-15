3b7afa91c341377d8a46a4963335d43a6e0aca7c
Madi Ryan <madi.ryan92@gmail.com>
2019-11-14 20:15:26 -0600

    recursion practice problems

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-///A recursive function is a function that invokes itself
-//write a function that calls the string "x" amounts of times
-function writePsychoBook(num) {
-  if (num === 0) {
-    return ""; // if num====1 {return""}  is the exit condition
-  }
-  return `All work and no play makes jack a dull boy ${writePsychoBook(
-    num - 1
-  )}`;
-}
+//console.log(Math.pow(2, 3)); //Math.pow is for exponents= 2*2*2
+function myPow(num, exp) {
+  let ret = 1;
 
-console.log(writePsychoBook(10));
+  for (let i = 1; i < exp; i += 1) {
+    ret *= num;
+  }
+  return ret;
+}
+console.log(myPow(2, 3));
+
+//recursion
+
+function myPow(num, exp) {
+  if (exp === 0) {
+    return 1;
+  }
+  return (num *= myPow(num, exp - 1));
+}
+console.log(myPow(2, 3));
