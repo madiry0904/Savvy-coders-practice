7f2a83d0bb6bc7b7f6cf83768ed32badc418e33b
Madi Ryan <madi.ryan92@gmail.com>
2019-11-15 21:16:29 -0600

    add Array for students and output

--- random-student.js
+++ random-student.js
@@ -21,3 +21,11 @@ const myArray = [
   "Tracie",
   "Wallat"
 ];
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+console.log(myArray[getRandomInt(0, myArray.length - 1)]);
