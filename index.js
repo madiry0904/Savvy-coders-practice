09c63ebd9c8e387d4f8fbf0b5fea8624c30b2ae9
Madi Ryan <madi.ryan92@gmail.com>
2019-11-15 18:41:57 -0600

    example of function factory

--- index.js
+++ index.js
@@ -1,44 +1,31 @@
-//pass by reference vs pass by value aka pass by copy
-//Heap is an area in memory where values "hang out"
+//FIZZ BUZZ
+// If we pass a number that's divisible by both 5 and 3 the log 'fizzbuzz'
+//If we pass a number divisible by 3, log 'fizz
+//If we pass a number divisible by 5, log 'buzz'
+//Loop over numbers from 1-99
+//If not divisible by either 5 or 3, log the number
 
-//PASS BY VALUE(PRIMITIVES aka strings booleans numbers....)
-
-//Primitives(which numbers are) can't be mutated
-//Primitives are passed by value
-let x = 3;
-
-let y = x;
-
-function add1(num) {
-  return num + 1;
+/*function fizzBuzz(num){
+  for (let i = 1; i <= 99; i += 1){
+    if (i % 3 ===0 && i % 5 ===0){
+      console.log("fizzbuzz");
+    }else if (i % 5 === 0){
+      console.log("buzz")
+    }else if (i !%)
+  }
 }
+*/
 
-console.log(add1(x));
-console.log(y);
-
-//returns 4
-//returns 3 because it's a "copy"- they're not interlinked
-
-//y references it's own COPY of the number 3; they're not directly linked
-// x THROWS AWAY it's '3' values when passed through the function and gets a whole new value of 4 because the function says n+1 but y keeps it's value of 3 because y made a copy-the two (x, y) aren't interrelated
-
-//PASS BY REFERENCE(objects can be mutated)
-const me = {
-  name: "Mark"
-};
-
-const meToo = me;
-
-function nameChanger(obj, newName) {
-  obj.name = newName;
+//FUNCTION FACTORY PART 2- FUNCTIONS THAT RETURN OTHER FUNCTIONS
+function initPerson(fname, lname, age) {
+  return {
+    firstName: fname,
+    lastName: lname,
+    age,
+    canDrink: function() {
+      return age >= 21;
+    }
+  };
 }
-
-nameChanger(me, "Fred");
-
-console.log(me);
-console.log(meToo);
-//outputs Fred/Fred (because they're interlinked)
-
-//I pass in 'me' to nameChanger
-//think like they're sharing the same name
-//whatever happens to one object happens to the other one (unlike pass by value where it's basically making a copy)
+const me = initPerson("madi", "ryan", 13);
+console.log(me.canDrink());
---
+++ index2.js
@@ -0,0 +1,20 @@
+function sayHello(name, greeting) {
+  return `Hello ${name} ${greeting}`;
+}
+
+console.log(sayHello("Madi", "How are you today?"));
+//Simply create some variables using const and let.
+//Give at least 3 strings and 3 numbers.
+//In the case of let display that you understand
+//how to reassign variables by reusing the
+//same variable identifier (name).
+
+let husbandName = "George";
+husbandName = "Jack";
+
+let favoriteFruit = "apples";
+let favoriteActivity = "hiking";
+
+const age = 23;
+const nieceAge = 2 + 5;
+const apartmentAge = 0;
