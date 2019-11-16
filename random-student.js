f30a3d74df679e1762229cec4c01f215127055cc
Madi Ryan <madi.ryan92@gmail.com>
2019-11-15 21:04:13 -0600

    array of student names for project

---
+++ random-student.js
@@ -0,0 +1,23 @@
+/*ARRAYS ARE OBJECTS->they're composite data type
+//arrays have auto incremented numerical keys (aka index)
+
+const myArray = [];
+//vs
+const myObj = {};
+*/
+
+const myArray = [
+  "Andrew",
+  "Billy",
+  "George",
+  "Heather",
+  "Madison",
+  "Marque",
+  "Martin",
+  "Nikki",
+  "Oscar",
+  "RhondaLyn",
+  "Sidney",
+  "Tracie",
+  "Wallat"
+];
