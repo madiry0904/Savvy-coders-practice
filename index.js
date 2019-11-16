abc4aeba976929eb7f65bed4fa60693fa317fc9c
Madi Ryan <madi.ryan92@gmail.com>
2019-11-15 19:29:23 -0600

    for/while/recursionpractice 11.15.19

--- index.js
+++ index.js
@@ -1,31 +1,33 @@
-//FIZZ BUZZ
-// If we pass a number that's divisible by both 5 and 3 the log 'fizzbuzz'
-//If we pass a number divisible by 3, log 'fizz
-//If we pass a number divisible by 5, log 'buzz'
-//Loop over numbers from 1-99
-//If not divisible by either 5 or 3, log the number
+//USING WHILE LOOP
 
-/*function fizzBuzz(num){
-  for (let i = 1; i <= 99; i += 1){
-    if (i % 3 ===0 && i % 5 ===0){
-      console.log("fizzbuzz");
-    }else if (i % 5 === 0){
-      console.log("buzz")
-    }else if (i !%)
+function logNumbers(num) {
+let count = 1;
+while (count <= num)
+console.log(count)
+count += 1;
+}
+logNumbers(10);
+
+
+//USING FOR LOOP(COUNT TO 10)
+
+function logNumbers(num) {
+  for (let i = 1; i <= num; i += 1) {
+  console.log(i);
+}
+}
+logNumbers(10);
+
+
+//RECURSION-> usually a countdown so checking if the thing is already at 1
+//recursive function requires a return function
+//Recursion involves counting backwards or looping backwards so we start with the end in mind
+//In this case our exit condition is when we have reached 1
+
+function logNumbers(num) {
+  if (num === 1) {      //This first section is the exit condition
+    return 1;
   }
+  return num + logNumbers(num - 1); //this is telling you how to actually get the # you want                                            (whatever num is currently add that number -1
 }
-*/
-
-//FUNCTION FACTORY PART 2- FUNCTIONS THAT RETURN OTHER FUNCTIONS
-function initPerson(fname, lname, age) {
-  return {
-    firstName: fname,
-    lastName: lname,
-    age,
-    canDrink: function() {
-      return age >= 21;
-    }
-  };
-}
-const me = initPerson("madi", "ryan", 13);
-console.log(me.canDrink());
+console.log(logNumbers(10));
