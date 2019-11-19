cacb256884cf5f78bb64ae562a5567811b5e1439
Madi Ryan <madi.ryan92@gmail.com>
2019-11-18 18:53:43 -0600

    actual fizzbuzz example and fizzbuzz homework 5 answers

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
+//11.18.19 FIZZBUZZ REVIEW
+//FUNCTION DECLARATION not Expression (const fizzbuzz = function () {})
+
+function fizzBuzz() {
+  for (let i = 1; i <= 100; i += 1) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
+  }
+}
+fizzBuzz();
+
 function initFizzBuzz(num1, num2) {
-  return function fizzBuzz46() {
-    for (let i = 1; i <= 20; i += 1) {
+  return function() {
+    for (let i = 1; i <= 100; i += 1) {
       if (i % num1 === 0 && i % num2 === 0) {
         console.log("FizzBuzz");
-      } else if (i % num1 === 0 && i % num2 !== 0) {
+      } else if (i % num1 === 0) {
         console.log("Fizz");
-      } else if (i % num1 !== 0 && i % num2 === 0) {
+      } else if (i % num2 === 0) {
         console.log("Buzz");
       } else {
         console.log(i);
       }
     }
   };
-  return initFizzBuzz();
 }
-
 const fizzBuzz46 = initFizzBuzz(4, 6);
-
 fizzBuzz46();
