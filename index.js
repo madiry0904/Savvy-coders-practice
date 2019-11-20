5bf713bf7604787dcd0bf721cca8ef3fc3391365
Madi Ryan <madi.ryan92@gmail.com>
2019-11-19 18:55:13 -0600

    adding notes about function constructors and OOP 11.19.19

--- index.js
+++ index.js
@@ -1,23 +1,12 @@
-//Function Constructor
-function manageYourHealth(name, stressLevel, fitnessLevel) {
-  this.name = name;
-  this.stressLevel = stressLevel;
-  this.fitnessLevel = fitnessLevel;
+/*
+ ** if you want to return an object use function constructors
+ ** if want to return function use function factory
+ ** JS  is a prototype OOP while other languages are class based OOP languages
+ ** getter and setter are basically functions to "protect" the data
+    ** setter is something where we have a method that will manipulate the data
+        if you want to change someones last name without going in you create a method to change
 
-  this.manageStress = function() {
-    if (this.stressLevel >= 7 && this.fitnessLevel >= 7) {
-      return `Awesome job ${this.name} on your fitness! Now let's manage your stress levels because a stress level of ${this.stressLevel} is high! Your task for this week is to meditate 5 times a week for 5 minutes.`;
-    }
-    return `Wow you're killing it! Keep up the amazing work ${this.name}!`;
-  };
-  this.startExercise = function() {
-    if (this.stressLevel <= 7 && this.fitnessLevel <= 3) {
-      return `Great job on managing your stress ${this.name}! Now let's get you a consistent fitness regime because a stress level of ${this.stressLevel} is great but a fitness level of ${this.fitnessLevel} needs improvement. Your task is to go for a walk for 20 minutes 3 times a week.`;
-    }
-    return `Awesome job ${this.name} you're doing fantastic!`;
-  };
-}
 
-const me = new manageYourHealth("Grace", 0, 2);
-console.log(me.manageStress());
-console.log(me.startExercise());
+  ** must use the "new" keyword for a function constructor because it actually returns out an instance of an object
+      ** const me = new Person ('Manav', 'Misra, 23);
+*/
