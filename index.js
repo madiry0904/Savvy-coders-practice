b866e54f1b20c994ab840a2ee36d8b82ba684a76
Madi Ryan <madi.ryan92@gmail.com>
2019-11-19 20:07:57 -0600

    error checking examples 11.19.19

--- index.js
+++ index.js
@@ -1,12 +1,65 @@
-/*
- ** if you want to return an object use function constructors
- ** if want to return function use function factory
- ** JS  is a prototype OOP while other languages are class based OOP languages
- ** getter and setter are basically functions to "protect" the data
-    ** setter is something where we have a method that will manipulate the data
-        if you want to change someones last name without going in you create a method to change
+function Person(fname, lname, age) {
+  this.fname = fname;
+  this.lname = lname;
+  this.age = age;
 
+  this.getFullName = function() {
+    return `${this.fname} ${this.lname}`;
+  };
 
-  ** must use the "new" keyword for a function constructor because it actually returns out an instance of an object
-      ** const me = new Person ('Manav', 'Misra, 23);
-*/
+  this.setLastName = function(newLastName) {
+    if (!newLastName) {
+      console.error("no blank last name allowed");
+      return;
+    }
+    this.lname = newLastName;
+  };
+}
+
+const older = new Person("John", "Smith", 77);
+
+console.log("older says", older);
+console.log(older.getFullName());
+
+older.setLastName();
+console.log("older says", older);
+
+function manageYourHealth(name, stressLevel, fitnessLevel) {
+  this.name = name;
+  this.stressLevel = stressLevel;
+  this.fitnessLevel = fitnessLevel;
+
+  this.manageStress = function() {
+    if (this.stressLevel <= 7 && this.fitnessLevel >= 7) {
+      return `Awesome job ${this.name} on your fitness! Let's focus on managing your stress levels because a stress level of ${this.stressLevel} is too high!`;
+    }
+    return `Wow you're doing awesome! Keep up the good work!`;
+  };
+  this.startExercise = function() {
+    if (this.stressLevel <= 7 && this.fitnessLevel <= 3) {
+      return `Great job on managing your stress ${this.name}!`;
+    }
+    return `Awesome job ${this.name} you're doing fantastic!`;
+  };
+  this.newFname = function(newFirstName) {
+    if (!newFirstName) {
+      console.log("no blank first names allowed");
+      return;
+    }
+    this.name = newFirstName;
+  };
+
+  this.setStressLevel = function(stress2) {
+    if (stress2 && typeof stress2 === "number") {
+      this.stressLevel = stress2;
+    } else {
+      console.error("not a number");
+    }
+  };
+}
+
+const me = new manageYourHealth("Annabel", 0, 2);
+//console.log(me.manageStress());
+//console.log(me.startExercise());
+//me.newFname();
+me.setStressLevel("Hello");
