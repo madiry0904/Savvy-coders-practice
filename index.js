570e3a40fdcab4c4a35e3ce1410df15a82575ed2
Madi Ryan <madi.ryan92@gmail.com>
2019-11-23 16:58:33 -0600

    homework 6 practice 11.23.19

--- index.js
+++ index.js
@@ -1,37 +1,62 @@
-//Using Class
-class Person {
-  constructor(name, age, job) {
-    this.name = name;
-    this.age = age;
-    this.job = job;
-  }
-  get firstName() {
-    return `My name is ${this.name}!`;
-  }
-  get bio() {
-    return `Hi my name is ${this.name}. I am a ${this.age} year old ${this.job}`;
-  }
-  set changeName(fname) {
-    if (fname) {
-      this.fname = fname;
-    } else {
-      console.error("bad name received");
-    }
-  }
+/*function bBallTeam(name, city) {
+  this.name = name;
+  this.city = city;
 }
-class Employee extends Person {
-  constructor(name, age, job, empid) {
-    super(name, age, job);
-    this.id = empid;
+bBallTeam.prototype.setNumbers = function(numArray) {
+  this.numbers = numArray;
+};
+bBallTeam.prototype.printNums = function() {
+  for (let i = 0; i <= this.numbers.length; i += 1) {
+    console.log(this.numbers[i]);
   }
-  get bio() {
-    return `I love my job!`;
-  }
-}
-const me = new Person("Madi", 27, "AmeriCorps Vista");
-const emp = new Employee("Madi", 27, "AmeriCorps Vista", 123);
-//console.log(me.firstName);
-//console.log(me.changeName);
+};
+const bulls96 = new bBallTeam("Bulls", "Chicago");
+bullsNumbers = [23, 91, 33, 13, 9, 25];
+bulls96.setNumbers(bullsNumbers);
 
-console.log((me.changeName = "Kelly"));
-console.log(emp);
+bulls96.printNums();
+*/
+
+function healthy(name, fitness, stress) {
+  this.name = name;
+  this.fitness = fitness;
+  this.stress = stress;
+}
+healthy.prototype.getFitnessLevel = function() {
+  if (this.fitness >= 7) {
+    return `great job on your fitness ${this.name}! Now let's focus on your stress!`;
+  }
+  return `A fitness level of ${this.fitness} is pretty low, let's focus on increasing your workouts this week!`;
+};
+
+healthy.prototype.getStressLevel = function() {
+  if (this.stress <= 5) {
+    return `Wow ${this.name} a stress level of ${this.stress} is fantastic!`;
+  }
+  return `A stress level of ${this.stress} is hurting your health! Let's focus on lowering your stress levels this week!`;
+};
+
+healthy.prototype.setFitnessLevel = function(newFitness) {
+  if (typeof newFitness === "number") {
+    this.fitness = newFitness;
+    return `fitness level changed`;
+  }
+  console.error(`invalid entry try again please`);
+};
+healthy.prototype.setCalorie = function(newArray) {
+  if (newArray) {
+    this.array = newArray;
+  }
+};
+
+const me = new healthy("Madi", 10, 2);
+calorieNumbers = [2000, 5000, 300, 7000];
+me.setCalorie(calorieNumbers);
+
+healthy.prototype.printNum = function() {
+  for (let i = 0; i <= this.array.length; i += 1) {
+    console.log(this.array[i]);
+  }
+};
+
+me.printNum();
