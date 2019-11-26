33ac705e6ca0f79314aeaadf03a5dab7efde0b43
Madi Ryan <madi.ryan92@gmail.com>
2019-11-25 19:39:30 -0600

    HW 6 solution

--- index.js
+++ index.js
@@ -1,48 +1,57 @@
-/*class Person {
-  constructor(first, last, age, job) {
-    this.first = first;
-    this.last = last;
-    this.age = age;
-    this.job = job;
-  }
-  get bio() {
-    return `Hi my name is ${this.first} ${this.last} and I work as a ${this.job}`;
-  }
-  get fullName() {
-    return `My name is ${this.first} ${this.last}`;
-  }
+import { POINT_CONVERSION_COMPRESSED } from "constants";
+
+function healthy(name, fitness, stress) {
+  this.name = name;
+  this.fitness = fitness;
+  this.stress = stress;
 }
-
-const me = new Person("Madi", "Ryan", 27, "AmeriCorps VISTA");
-//console.log(me.bio);
-console.log(me.fullName);
-*/
-class healthy {
-  constructor(name, fitness, stress) {
-    this.name = name;
-    this.fitness = fitness;
-    this.stress = stress;
+healthy.prototype.getFitnessLevel = function() {
+  if (this.fitness >= 7) {
+    return `great job on your fitness ${this.name}! Now let's focus on your stress!`;
   }
+  return `A fitness level of ${this.fitness} is pretty low, let's focus on increasing your workouts this week!`;
+};
 
-  get fitnessLevel() {
-    if (this.fitness >= 7) {
-      return `great job on your fitness ${this.name}! Now let's focus on your stress!`;
-    }
-    return `A fitness level of ${this.fitness} is too low, let's focus on upping your fitness days up!`;
+healthy.prototype.getStressLevel = function() {
+  if (this.stress <= 5) {
+    return `Wow ${this.name} a stress level of ${this.stress} is fantastic!`;
   }
+  return `A stress level of ${this.stress} is hurting your health! Let's focus on lowering your stress levels this week!`;
+};
 
-  get stressLevel() {
-    if (this.stress <= 5) {
-      return `Wow ${this.name} a stress level of ${this.stress} is fantastic!`;
-    }
-    return `A stress level of ${this.stress} is hurting your health! Let's focus on lowering your stress levels this week!`;
+healthy.prototype.setFitnessLevel = function(newFitness) {
+  if (typeof newFitness === "number") {
+    this.fitness = newFitness;
+    return `fitness level changed`;
   }
-
-  set calorie(newArray) {
+  console.error(`invalid entry try again please`);
+};
+healthy.prototype.setCalorie = function(newArray) {
+  if (newArray) {
     this.array = newArray;
   }
-}
-
+};
+healthy.prototype.getCalorie = function() {
+  let calList = `Here are some new goals ${this.name}`;
+  for (let i = 0; i < this.array.length; i += 1) {
+    calList += this.array[i];
+  }
+  return calList;
+};
+//newArray = [2000, 5000, 300, 7000];
 const me = new healthy("Madi", 10, 2);
-calories = [2000, 5000, 3000, 7000];
-console.log(calories.pop());
+me.setCalorie([2000, 4500, 7000, 10000]);
+console.log(me.getCalorie());
+
+//CLASS DOES NOT CHANGE THE FACT THAT WE ARE DOING PROTOTYPE-BASED OOP AND NOT CLASS-BASED OOP
+class Person {
+  constructor(fname, lname, age, occupation) {
+    this.fname = fname;
+    this.lname = lname;
+    this.age = age;
+    this.occupation = occupation;
+  }
+  get bio() {
+    return;
+  }
+}
