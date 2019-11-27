7d04734208f7a40894cde446d07fa10cf13cc59b
Madi Ryan <madi.ryan92@gmail.com>
2019-11-26 19:25:57 -0600

    array concepts

--- index.js
+++ index.js
@@ -1,100 +1,24 @@
-/*
-function healthy(name, fitness, stress) {
-  this.name = name;
-  this.fitness = fitness;
-  this.stress = stress;
-}
-healthy.prototype.getFitnessLevel = function() {
-  if (this.fitness >= 7) {
-    return `great job on your fitness ${this.name}! Now let's focus on your stress!`;
-  }
-  return `A fitness level of ${this.fitness} is pretty low, let's focus on increasing your workouts this week!`;
-};
+//Array Concepts
+//call an array by using const myArray = [];
 
-healthy.prototype.getStressLevel = function() {
-  if (this.stress <= 5) {
-    return `Wow ${this.name} a stress level of ${this.stress} is fantastic!`;
-  }
-  return `A stress level of ${this.stress} is hurting your health! Let's focus on lowering your stress levels this week!`;
-};
+const numbers = [1, 2, 3, 4];
+const strings = ["hi", "world"];
 
-healthy.prototype.setFitnessLevel = function(newFitness) {
-  if (typeof newFitness === "number") {
-    this.fitness = newFitness;
-    return `fitness level changed`;
-  }
-  console.error(`invalid entry try again please`);
-};
-healthy.prototype.setCalorie = function(newArray) {
-  if (newArray) {
-    this.array = newArray;
-  }
-};
-healthy.prototype.getCalorie = function() {
-  let calList = `Here are some new goals ${this.name}`;
-  for (let i = 0; i < this.array.length; i += 1) {
-    calList += this.array[i];
-  }
-  return calList;
-};
-//newArray = [2000, 5000, 300, 7000];
-const me = new healthy("Madi", 10, 2);
-me.setCalorie([2000, 4500, 7000, 10000]);
-console.log(me.getCalorie());
+const numbersAndStrings = numbers.concat(strings);
 
-*/
-//CLASS DOES NOT CHANGE THE FACT THAT WE ARE DOING PROTOTYPE-BASED OOP AND NOT CLASS-BASED OOP
-class Person {
-  constructor(fname, lname, age, occupation) {
-    this.fname = fname;
-    this.lname = lname;
-    this.age = age;
-    this.occupation = occupation;
-  }
-  get fullName() {
-    return `${this.fname} ${this.lname}`;
-  }
-  set changeName(newLName) {
-    if (newLName) {
-      this.lname = newLName;
+function updatedNumbersAndStrings(originalArray, stringUpdate, numberUpdate) {
+  const updatedNumbersAndStrings = [];
+  for (let i = 0; i < originalArray.length; i += 1) {
+    if (typeof originalArray[i] === "string") {
+      updatedNumbersAndStrings.push((originalArray[i] += stringUpdate));
     } else {
-      console.error("Name not changed");
+      updatedNumbersAndStrings.push((originalArray[i] += numberUpdate));
     }
   }
-}
-const me = new Person("George", "Blezard", 28, "Teacher");
-console.log(me.fullName);
-me.changeName = "Ryan";
-console.log(me);
-
-class Employee extends Person {
-  constructor(fname, lname, age, occupation, empid) {
-    super(fname, lname, age, occupation); //uses information from Person constructor (this.age etc)
-    this.id = empid;
-  }
-  get empBio() {
-    return `My name is ${this.fname} ${this.lname} and my employee id is ${this.id}`;
-  }
-  set empBio(empid) {
-    this.id = empid;
-  }
-  get assignments() {
-    let ret = `The current task list is as follows:`;
-
-    for (let i = 0; i < this.workload.length; i += 1) {
-      ret += `${this.workload[i]}`;
-    }
-    return ret;
-  }
-  set assignments(assignments) {
-    this.workload = assignments;
-  }
+  return updatedNumbersAndStrings;
 }
 
-const emp = new Employee("Madi", "Ryan", 27, "Program Analyst", 1234);
-console.log(emp);
-emp.assignment = ["pick up the trash", "eat a bagel", "clear whiteboard"];
-console.log(emp);
-
-//with classes you always need a getter and a setter
-//getters usually have strings where setters set things
+console.log(updatedNumbersAndStrings(numbersAndStrings, "HELLO", 275));
+//console.log("numbers is", numbers);
+//console.log("strings is", strings);
+//console.log("numbersAndStrings is", numbersAndStrings);
