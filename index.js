74aa2d9c336e1c234b6b83acd69eb7ebb28a3c1c
Madi Ryan <madi.ryan92@gmail.com>
2019-12-05 20:27:44 -0600

    more practice with filtering

--- index.js
+++ index.js
@@ -20,16 +20,30 @@ function doubleAndAdd1(n) {
 const numsArrayAddedOneAndDoubled = nums.map(anyNameIWant =>
   add1AndDouble(anyNameIWant)
 );
-console.log(numsArrayAddedOneAndDoubled);
+//console.log(numsArrayAddedOneAndDoubled);
 
 //TODO: recreate numsArrayAddedOneAndDoubled only using 1st two functions
 
 //This is functional composition
 const addTwoArrays = nums.map(num => add1(num)).map(num2 => double(num2));
-console.log(addTwoArrays); ///this works or you can just log directly aka below
+//console.log(addTwoArrays); ///this works or you can just log directly aka below
 //console.log(nums.map(num => double(num)).map(num2 => add1(num2)));
 
 // Pure function are when you don't have to grab something outside of the scope- no console.log in them
+
 //Pure functions are copy and paste functions and have an explicit return
 
+//Functional programming employs function composition with pure functions
+//filter must return a boolean
+
+//TODO :filter out all odd numbers
+const results = nums.filter(num => num % 2 === 1);
+console.log(results);
+
 //TODO: Filter out all odd numbers and then repeat
+const addOddNumbers = nums
+  .filter(num => num % 2 == 1)
+  .map(num => add1(num))
+  .map(num2 => double(num2));
+
+console.log(addOddNumbers);
