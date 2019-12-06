1268d3399234445c9b8f0ef6f05fdda6fcaf6b8c
Madi Ryan <madi.ryan92@gmail.com>
2019-12-05 20:55:56 -0600

    array superpower reduce

--- index.js
+++ index.js
@@ -1,49 +1,16 @@
+//TODO: Use reduce to sum up these numbers.
+//reduce "reduces" an array down to one or accumulates everything into 1
+//must have a total and something to reference the current num for reduce
 const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const sum = nums.reduce((total, currentNum) => (total += currentNum));
+//console.log(sum);
 
-function add1(n) {
-  return n + 1;
+//TODO: Write a function that takes an Array of Numbers and returns...the average
+function getAvg(numbers) {
+  return (
+    numbers.reduce((total, currentNum) => (total += currentNum)) /
+    numbers.length
+  );
 }
-
-function double(n) {
-  return n * 2;
-}
-
-function add1AndDouble(n) {
-  return double(add1(n));
-}
-
-function doubleAndAdd1(n) {
-  return add1(double(n));
-}
-
-//TODO: Add 1 and double each num inside of nums
-const numsArrayAddedOneAndDoubled = nums.map(anyNameIWant =>
-  add1AndDouble(anyNameIWant)
-);
-//console.log(numsArrayAddedOneAndDoubled);
-
-//TODO: recreate numsArrayAddedOneAndDoubled only using 1st two functions
-
-//This is functional composition
-const addTwoArrays = nums.map(num => add1(num)).map(num2 => double(num2));
-//console.log(addTwoArrays); ///this works or you can just log directly aka below
-//console.log(nums.map(num => double(num)).map(num2 => add1(num2)));
-
-// Pure function are when you don't have to grab something outside of the scope- no console.log in them
-
-//Pure functions are copy and paste functions and have an explicit return
-
-//Functional programming employs function composition with pure functions
-//filter must return a boolean
-
-//TODO :filter out all odd numbers
-const results = nums.filter(num => num % 2 === 1);
-console.log(results);
-
-//TODO: Filter out all odd numbers and then repeat
-const addOddNumbers = nums
-  .filter(num => num % 2 == 1)
-  .map(num => add1(num))
-  .map(num2 => double(num2));
-
-console.log(addOddNumbers);
+//console.log(getAvg(nums));
+//TODO: Tally up the count for each other TLD aka .net/.com etc
