1b3b2eadf81d81cd9fc38e15632b70b0dd5186e4
Madi Ryan <madi.ryan92@gmail.com>
2019-12-09 20:22:54 -0600

    practice using filter, map, includes

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const strs = [
   "world",
   "hello"
 ];
-//write a fxn that takes an array and returns boolean representing if the word "hello" exists in the array and then lets see if there's anything in that new array with the true "hello"
+//TODO: write a fxn that takes an array and returns boolean representing if the word "hello" exists in the array and then lets see if there's anything in that new array with the true "hello"
 //this example is the long way to do it
 
 function booleanHello(dataSet) {
@@ -19,5 +19,12 @@ console.log(booleanHello(strs));
 
 //the above says go into the dataset, map over the dataset for the keyword "hello" (will return booleans) then filter over to pull out the ones that do say hello. Use .length to see how many are in the new array
 
-//includes does the above function
+//includes keyword does the above function -> uses map and filter not length so just seeing if the funciton includes hello
 console.log(strs.includes("hello"));
+
+//TODO: write a function that takes an array of values and a target value and returns how many times that target value exists in the array
+
+function countOccurences(dataSet, target) {
+  return dataSet.filter(d => d === target).length;
+}
+console.log(countOccurences(strs, "hello"));
