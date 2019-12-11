f50bcaf8d0beb7ae0c23998dfe037009a39bfe40
Madi Ryan <madi.ryan92@gmail.com>
2019-12-10 21:17:46 -0600

    add examples of functions to go in the library

--- index.js
+++ index.js
@@ -1,35 +1,8 @@
-const students = [
-  {
-    name: "Jane",
-    score: 99
-  },
-  {
-    name: "Joe",
-    score: 88
-  },
-  { name: "Mark", score: 71 },
-  { name: "Sheamus", score: 55 },
-  { name: "Sanjay", score: 77 }
-];
-
-/**
- * TODO: Build a function that takes in an Array of people with scores and a specified threshold,
- * and gives back a list of NAMES ONLY of the ones who scored less than that threshold.
- *
- * HINTS:
- * 1. Try filtering out based on if the SCORE is less than the threshold.
- * 2. Chain a map() to map over those filtered results but only return the 'name'.
- * 3. You can use OBJECT DESTRUCTURING...or not.
- */
-function getLowScores(people, threshold) {
-  return people.filter(p => p.score <= threshold).map(({ name }) => name);
-}
-// OR USE DESTRUCTURING LIKE BELOW
-function getScores(people, threshold) {
-  return people
-    .filter(({ score }) => score <= threshold)
-    .map(({ name }) => name);
+//TODO: write practice functions for the app
+function curveScores(originalScore, curveAmount) {
+  return originalScore + curveAmount;
 }
 
-console.log(getLowScores(students, 80));
-console.log(getScores(students, 80));
+function curveScoresForAllStudents(students, curveAmt) {
+  return students.map(({score} => (score += curveAmt ))
+}
